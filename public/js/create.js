826b5ede626d1c552751be5a15deeec8769d56e0
shubhamsc <s.chauhan5505@gmail.com>
2018-01-20 15:40:07 +0530

    added form to create todo

--- public/js/create.js
+++ public/js/create.js
@@ -1,31 +1,13 @@
 const loadPage=function(){
   document.getElementById('addItem').onclick=addItem;
-  document.getElementById('submit').onclick=submitTodo;
 }
 const addItem=function(){
-  let list = document.createElement('li');
-  list.id="item";
-  let item=document.getElementById('itemBox').value;
-  if(!item) return;
-  document.getElementById('itemList').appendChild(list).innerText=item;
-  document.getElementById('itemBox').value = "";
+  let input = document.createElement('input');
+  let br = document.createElement('br');  
+  input.name="item";
+  input.type="text";
+  document.getElementById('itemList').appendChild(input);
+  document.getElementById('itemList').appendChild(br)
 }
 
-const getItems=function(){
-  let allItems=document.querySelectorAll('#item');
-  let items = [];
-  for (let index = 0; index < allItems.length; index++) {
-    items.push(allItems[index].innerText);
-  }
-  return items;
-}
-const submitTodo=function(){
-  let title=document.querySelector('#title').value;
-  let desc=document.querySelector('#desc').value;
-  let items=getItems();
-  let xmlReq=new XMLHttpRequest();
-  xmlReq.open('POST','post_addTodo');
-  let data=`title=${title}&desc=${desc}&items=${items}`;
-  xmlReq.send(data);
-}
 window.onload=loadPage;
